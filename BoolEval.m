3e4cee970cbef52c72b4ba9f67a35fe1141fb9ee
Szabolcs Horvát <szhorvat@gmail.com>
2014-05-30 09:48:10 -0400

    Initial commit

---
+++ BoolEval.m
@@ -0,0 +1,45 @@
+(* Mathematica Package *)
+(* :Context: BoolEval` *)
+(* :Author: szhorvat *)
+(* :Date: 2014-05-30 *)
+
+BeginPackage["BoolEval`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+BoolEval::usage = "BoolEval[condition]"
+BoolPick::usage = "BoolPick[array, condition]"
+
+Begin["`Private`"] (* Begin Private Context *)
+
+greatereq[a_, b_] := UnitStep[a - b]
+lesseq[a_, b_] := UnitStep[b - a]
+greater[a_, b_] := 1 - lesseq[a, b]
+less[a_, b_] := 1 - greatereq[a, b]
+unequal[a_, b_] := Unitize[a - b]
+equal[a_, b_] := 1 - unequal[a, b]
+
+equal[a_, b_, c__] := equal[a, b] equal[b, c]
+less[a_, b_, c__] := less[a, b] less[b, c]
+greater[a_, b_, c__] := greater[a, b] greater[b, c]
+lesseq[a_, b_, c__] := lesseq[a, b] lesseq[b, c]
+greatereq[a_, b_, c__] := greatereq[a, b] greatereq[b, c]
+
+unequal[a__] := Times @@ (unequal @@@ Subsets[{a}, {2}])
+
+
+BoolEval[condition_] :=
+    Unevaluated[condition] /. {Less -> less, Greater -> greater,
+      LessEqual -> lesseq, Equal -> equal, Unequal -> unequal,
+      GreaterEqual -> greatereq, Or -> Composition[Unitize, Plus],
+      And -> Times, Not -> (1 - # &), True -> 1, False -> 0}
+
+
+BoolPick[array_, condition_] :=
+    Pick[array,
+      BoolEval[condition],
+      1
+    ]
+
+End[] (* End Private Context *)
+
+EndPackage[]
