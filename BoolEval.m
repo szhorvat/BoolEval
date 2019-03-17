7867cebd1a879b66e746d220b400ce27842057b9
Szabolcs Horvát <szhorvat@gmail.com>
2019-03-17 10:56:11 +0100

    Cleanup and fix Inequality processing

--- BoolEval.m
+++ BoolEval.m
@@ -6,31 +6,36 @@
 BeginPackage["BoolEval`"]
 (* Exported symbols added here with SymbolName::usage *)
 
-BoolEval::usage = "BoolEval[array > value] replaces elements of the numerical array 'array' which are greater than 'value' with 1, and the rest with 0.
-BoolEval[condition] takes a condition expressed in terms of >, >=, <, <=, ==, != and logical operators, and evaluates it for each element of the arrays appearing in the condition. The result is returned as a Boolean array of 0s and 1s."
-BoolPick::usage = "BoolPick[array, condition] will return the elements of a numerical array for which condition is True."
-BoolCount::usage = "BoolCount[condition] counts the number of elements satisfying the array condition."
+BoolEval::usage =
+    "BoolEval[array > value] replaces elements of the numerical array 'array' which are greater than 'value' with 1, and the rest with 0.\n" <>
+    "BoolEval[condition] takes a condition expressed in terms of >, >=, <, <=, ==, != and logical operators, and evaluates it for each element of the arrays appearing in the condition. The result is returned as a Boolean array of 0s and 1s.";
+
+BoolPick::usage =
+    "BoolPick[array, condition] will return the elements of a numerical array for which condition is True.";
+
+BoolCount::usage =
+    "BoolCount[condition] counts the number of elements satisfying the array condition.";
 
 Begin["`Private`"] (* Begin Private Context *)
 
-greatereq[a_, b_] := UnitStep@Subtract[a, b]
-lesseq[a_, b_] := UnitStep@Subtract[b, a]
-greater[a_, b_] := Subtract[1, lesseq[a, b]]
-less[a_, b_] := Subtract[1, greatereq[a, b]]
+greaterEq[a_, b_] := UnitStep@Subtract[a, b]
+lessEq[a_, b_] := UnitStep@Subtract[b, a]
+greater[a_, b_] := Subtract[1, lessEq[a, b]]
+less[a_, b_] := Subtract[1, greaterEq[a, b]]
 unequal[a_, b_] := Unitize@Subtract[a, b]
 equal[a_, b_] := Subtract[1, unequal[a, b]]
 
 equal[a_, b_, c__] := equal[a, b] equal[b, c]
 less[a_, b_, c__] := less[a, b] less[b, c]
 greater[a_, b_, c__] := greater[a, b] greater[b, c]
-lesseq[a_, b_, c__] := lesseq[a, b] lesseq[b, c]
-greatereq[a_, b_, c__] := greatereq[a, b] greatereq[b, c]
+lessEq[a_, b_, c__] := lessEq[a, b] lessEq[b, c]
+greaterEq[a_, b_, c__] := greaterEq[a, b] greaterEq[b, c]
 
 unequal[a__] := Times @@ (unequal @@@ Subsets[{a}, {2}])
 
 rules = Dispatch[{
-    Less -> less, LessEqual -> lesseq,
-    Greater -> greater, GreaterEqual -> greatereq,
+    Less -> less, LessEqual -> lessEq,
+    Greater -> greater, GreaterEqual -> greaterEq,
     Equal -> equal, Unequal -> unequal,
     Or -> (Unitize@Plus[##]&), And -> Times, Not -> (Subtract[1, #] &),
     Nor -> (Subtract[1, Unitize@Plus[##]]&), Nand -> (Subtract[1, Times[##]]&),
@@ -41,7 +46,7 @@ rules = Dispatch[{
 
 (* Convert Inequality expressions to canonical form, e.g. a < b > c  ==>  a < b && b > c *)
 ineq = Dispatch[{
-    HoldPattern@Inequality[a_, op_, b_] :> True,
+    HoldPattern@Inequality[a_, op_, b_] :> op[a, b],
     HoldPattern@Inequality[a_, op_, b_, rest__] :> op[a, b] && Inequality[b, rest]
   }];
 
